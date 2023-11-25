30abe19c6c0eac9567f53a1fec8478781fc06871
João Vitor Senha Dos Santos <joao.santos1381@etec.sp.gov.br>
2023-11-25 14:03:13 -0300

    menu

--- site/script.js
+++ site/script.js
@@ -1,3 +1,14 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.menu').style.display = 'block';
+document.addEventListener('DOMContentLoaded', function() {
+    var menuButton = document.querySelector('.hamburger');
+    var overlay = document.getElementById('overlay');
+
+    menuButton.addEventListener('click', function() {
+        document.body.classList.toggle('menu-open');
+        overlay.classList.toggle('active');
+    });
+
+    overlay.addEventListener('click', function() {
+        document.body.classList.remove('menu-open');
+        overlay.classList.remove('active');
+    });
 });
